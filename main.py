0fa5320ab5bce4d16a0ec0da3357083957eba89a
Hugo Filipe Matos Almeida Moreira <122613212+benderpt@users.noreply.github.com>
2024-10-10 10:21:50 +0000

    v0.1

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import fitz  # PyMuPDF
 import difflib
 
 def extract_text_from_pdf(pdf_path):
+    """
+    Extracts text from each page of a PDF file.
+
+    Args:
+        pdf_path (str): The file path to the PDF document.
+
+    Returns:
+        str: The extracted text from the PDF.
+    """
     # Abre o PDF e extrai o texto de cada página
     doc = fitz.open(pdf_path)
     text = ""
@@ -11,6 +20,16 @@ def extract_text_from_pdf(pdf_path):
     return text
 
 def compare_pdfs(pdf1_path, pdf2_path):
+    """
+    Compares the text content of two PDF files and returns the differences.
+
+    Args:
+        pdf1_path (str): The file path to the first PDF.
+        pdf2_path (str): The file path to the second PDF.
+
+    Returns:
+        list: A list of strings representing the differences between the two PDFs.
+    """
     # Extrai o texto dos dois PDFs
     text1 = extract_text_from_pdf(pdf1_path)
     text2 = extract_text_from_pdf(pdf2_path)
@@ -20,17 +39,27 @@ def compare_pdfs(pdf1_path, pdf2_path):
     return list(diff)
 
 def save_diff_to_file(diff, output_file):
-    with open(output_file, 'w') as f:
+    """
+    Saves the differences to a specified file.
+
+    Args:
+        diff (list of str): A list of strings representing the differences.
+        output_file (str): The path to the file where the differences will be saved.
+
+    Returns:
+        None
+    """
+    with open(output_file, 'w', encoding='utf-8') as f:
         for line in diff:
             f.write(line + '\n')
 
 # Define os caminhos dos PDFs e do ficheiro de saída
-pdf1_path = "c/workspaces/alteracoes_pdf/data/planapp_organica_2021.pdf"
-pdf2_path = "c/workspaces/alteracoes_pdf/data/planapp_organica_2024.pdf"
-output_file = "differences.txt"
+PDF1_PATH = "data/planapp_organica_2021.pdf"
+PDF2_PATH = "data/planapp_organica_2024.pdf"
+OUTPUT_FILE = "differences.txt"
 
 # Compara os PDFs e salva o resultado
-differences = compare_pdfs(pdf1_path, pdf2_path)
-save_diff_to_file(differences, output_file)
+differences = compare_pdfs(PDF1_PATH, PDF2_PATH)
+save_diff_to_file(differences, OUTPUT_FILE)
 
-print(f"Diferenças guardadas em {output_file}")
+print(f"Diferenças guardadas em {OUTPUT_FILE}")
